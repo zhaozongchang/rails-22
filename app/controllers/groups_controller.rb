95699e57cbee540ea1f2b5a98524b0b53e687d79
zhaozongchang <xingyingsuifeng@gmail.com>
2019-01-12 09:10:01 +0800

    baocun

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,19 +3,45 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
     redirect_to groups_path
+  else
+    render :new
+   end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+    redirect_to groups_path, alert: "更新成功"
+  else
+    render :edit
+   end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "删除成功"
   end
 
   private
 
   def group_params
     params.require(:group).permit(:title, :description)
-  end 
+  end
 end
