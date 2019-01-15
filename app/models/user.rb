82a38a168f10a7335e3eaef2466648d3856c14ea
zhaozongchang <xingyingsuifeng@gmail.com>
2019-01-15 09:37:50 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
    has_many :groups
    has_many :posts
+   has_many :participated_groups, :through => :group_relationships, :source => :group
 end
